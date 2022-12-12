133d61d5ff069843cd65d91a6f8978386d0ad127
maihd <maihd.dev@gmail.com>
2022-12-12 08:20:29 +0700

    add sub tests

---
+++ src/tests/math.zig
@@ -0,0 +1,31 @@
+const std = @import("std");
+const testing = std.testing;
+
+const math = @import("../math.zig");
+const types = @import("../types.zig");
+
+// Math unit tests
+
+test "math.f32x2" {
+    try testing.expectEqual(math.f32x2(1.0, 2.0), types.f32x2{ 1.0, 2.0 });
+}
+
+test "math.f32x3" {
+    try testing.expectEqual(math.f32x3(1.0, 2.0, 3.0), types.f32x3{ 1.0, 2.0, 3.0 });
+}
+
+test "math.f32x4" {
+    try testing.expectEqual(math.f32x4(1.0, 2.0, 3.0, 4.0), types.f32x4{ 1.0, 2.0, 3.0, 4.0 });
+}
+
+test "math.f32x2s" {
+    try testing.expectEqual(math.f32x2s(1.0), types.f32x2{ 1.0, 1.0 });
+}
+
+test "math.f32x3s" {
+    try testing.expectEqual(math.f32x3s(1.0), types.f32x3{ 1.0, 1.0, 1.0 });
+}
+
+test "math.f32x4s" {
+    try testing.expectEqual(math.f32x4s(1.0), types.f32x4{ 1.0, 1.0, 1.0, 1.0 });
+}
